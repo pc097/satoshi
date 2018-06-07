3db8bd35b24e436cc26965393ab6851ee9659890
RENATO <RENATO@RENATO-PC.spo.virtua.com.br>
2018-06-07 13:06:06 -0300

    Lote2 LP

---
+++ Lote2/src/SistemaAluno/Aluno.java
@@ -0,0 +1,21 @@
+package SistemaAluno;
+
+public class Aluno {
+
+    String pnome;
+    String unome;
+    int pontos;
+
+    // construtor	 
+    Aluno() {
+        this("", "", 0);
+    }
+
+    Aluno(String pnomealuno,
+            String unomealuno,
+            int pontosaluno) {
+        pnome = pnomealuno;
+        unome = unomealuno;
+        pontos = pontosaluno;
+    }
+}
---
+++ Lote2/src/SistemaAluno/LerAluno.java
@@ -0,0 +1,33 @@
+package SistemaAluno;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LerAluno {
+
+    public static void main(String[] args) throws IOException {
+        Aluno[] aluno = new Aluno[3];
+        int i;
+        String fileName = "ArquivoAluno.txt";
+
+        BufferedReader ler = new BufferedReader(new FileReader(fileName));
+
+        for (i = 0; i < 3; i++) {
+            aluno[i] = new Aluno();
+        }
+
+        for (i = 0; i < 3; i++) {
+            aluno[i].pnome = ler.readLine();
+            aluno[i].unome = ler.readLine();
+            aluno[i].pontos = Integer.parseInt(ler.readLine());
+        }
+
+        for (i = 0; i < 3; i++) {
+            System.out.println(aluno[i].pnome + " " + aluno[i].unome + " pontos: " + aluno[i].pontos);
+        }
+        ler.close();
+        System.exit(0);
+
+    }
+}
